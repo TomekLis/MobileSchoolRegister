752ccff06626541379f24f8a72f10bebc1cc670c
tmkls <listomek307@gmail.com>
2017-12-22 16:09:26 +0100

    Added another activity for displaing user data

--- app/src/main/java/com/example/tomasz/mobileschoolregister/LoginActivity.java
+++ app/src/main/java/com/example/tomasz/mobileschoolregister/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.tomasz.mobileschoolregister;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -365,7 +366,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 Toast.makeText(LoginActivity.this, "good the token is :" + responseToken.body().getAccessToken(), Toast.LENGTH_SHORT).show();
-
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
