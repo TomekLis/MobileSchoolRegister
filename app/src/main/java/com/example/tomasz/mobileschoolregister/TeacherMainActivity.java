6d465dd85984ba03f33d162f85341ca6cb414418
tmkls <listomek307@gmail.com>
2017-12-26 00:11:30 +0100

    few layout adjustements to prepare for creating lesson functionality

--- app/src/main/java/com/example/tomasz/mobileschoolregister/TeacherMainActivity.java
+++ app/src/main/java/com/example/tomasz/mobileschoolregister/TeacherMainActivity.java
@@ -38,12 +38,11 @@ public class TeacherMainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fabCreateLesson = (FloatingActionButton) findViewById(R.id.create_lesson);
+        fabCreateLesson.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                
             }
         });
 
